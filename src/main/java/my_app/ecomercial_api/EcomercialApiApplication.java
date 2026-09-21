40d59aca162b550921b34ad43af8ca61b2fa823f
Gia Bao <bao2004dt@gmail.com>
2026-09-21 13:59:41 +0700

    chore: initialize Spring Boot 3 project structure with core dependencies

---
+++ src/main/java/my_app/ecomercial_api/EcomercialApiApplication.java
@@ -0,0 +1,13 @@
+package my_app.ecomercial_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomercialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomercialApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my_app/ecomercial_api/EcomercialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package my_app.ecomercial_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomercialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
